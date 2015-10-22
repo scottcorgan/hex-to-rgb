75130b755ce0cfc54424ffc470df661c73488e77
Scott Corgan <scottcorgan@gmail.com>
2015-10-22 09:43:05 -0700

    expand shorthand hex to 6-digit hex. Fixes #1

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var hexToRgb = require('./index');
 var test = require('tape');
 
 test('converts', function (t) {
-  
+
   t.deepEqual(hexToRgb('000000'), [0,0,0], 'black converted');
   t.deepEqual(hexToRgb('000'), [0,0,0], 'black shorthand converted');
   t.deepEqual(hexToRgb('#ffffff'), [255,255,255], 'white converted with hash');
